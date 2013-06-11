b23ce5ad8ab674486e1b2c17b125d0f6b0cad53c
Wu Jie <KarasAya@gmail.com>
2013-06-11 17:55:16 +0800

    让texture info可以在_test项目中渲染

--- ex2d_dev/Assets/ex2D/Core/exTextureInfo.cs
+++ ex2d_dev/Assets/ex2D/Core/exTextureInfo.cs
@@ -18,14 +18,22 @@ using UnityEngine;
 ///////////////////////////////////////////////////////////////////////////////
 
 public class exTextureInfo : ScriptableObject {
-    // public string rawTextureGUID; // TODO: do we need this??
+    // public string rawTextureGUID;
     public Texture2D texture; ///< the atlas or raw texture
-
-    public int rawWidth = 1;
-    public int rawHeight = 1;
 
     public bool rotated = false; ///< if rotate the texture in atlas 
     public bool trim = false;    ///< if trimmed the texture
+
+    // for texture offset
+    public int trim_x = 0;
+    public int trim_y = 0;
+    public int rawWidth = 1;
+    public int rawHeight = 1;
+
+    public int x = 0;
+    public int y = 0;
+    public int width = 1;
+    public int height = 1;
 
     [System.NonSerialized] public float s0 = 0.0f;
     [System.NonSerialized] public float t0 = 0.0f;
