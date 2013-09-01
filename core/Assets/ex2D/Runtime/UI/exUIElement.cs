cf2e7dfe56712a0b19ee3789af53497104929c52
Johnny Wu <KarasAya@gmail.com>
2013-09-01 17:28:40 +0800

    update

--- core/Assets/ex2D/Runtime/UI/exUIElement.cs
+++ core/Assets/ex2D/Runtime/UI/exUIElement.cs
@@ -29,9 +29,47 @@ public class exUIElement {
     public Vector3 position;
     public Vector3 size;
 
-    public exUIElement parent;
-    public List<exUIElement> children;
+    [System.NonSerialized] protected exUIElement parent_ = null;
+    public exUIElement parent {
+        set {
+            if ( !ReferenceEquals(parent_, value) ) {
+                if ( !ReferenceEquals(parent_, null) ) {
+                    parent_.children.Remove(this);
+                    parent_ = null;
+                }
 
+                if ( !ReferenceEquals(value, null) ) {
+                    parent_ = value;
+                    if ( parent_.children.IndexOf (this) == -1 ) {
+                        parent_.children.Add(this);
+                    }
+                }
+            }
+        }
+        get {
+            return parent_;
+        }
+    }
+
+    public List<exUIElement> children = new List<exUIElement>();
+
+    // ------------------------------------------------------------------ 
+    // Desc: 
+    // ------------------------------------------------------------------ 
+
+    public void AddElement ( exUIElement _el ) {
+        _el.parent = this;
+    }
+
+    // ------------------------------------------------------------------ 
+    // Desc: 
+    // ------------------------------------------------------------------ 
+
+    public void RemoveElement ( exUIElement _el ) {
+        if ( children.IndexOf(_el) != -1 ) {
+            _el.parent = null;
+        }
+    }
 }
 
 
