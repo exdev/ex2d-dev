4295c5a698fcbc95ee483ade8bb60186eab1122c
Jare Guo <jare85@163.com>
2014-10-26 17:15:10 +0800

    add profiler window (ex2D/Debugger/Check Batching)

--- core/Assets/ex2D/Editor/CheckBatchingWindow.cs
+++ core/Assets/ex2D/Editor/CheckBatchingWindow.cs
@@ -12,12 +12,29 @@
 using UnityEditor;
 using UnityEngine;
 using System.IO;
+using System.Collections.Generic;
+using UnityEditor.AnimatedValues;
 
 ///////////////////////////////////////////////////////////////////////////////
 // defines
 ///////////////////////////////////////////////////////////////////////////////
 
 class CheckBatchingWindow : EditorWindow {
+
+    class LayerInfo {
+        public exLayer layer;
+        public int materials;
+        public List<BatchInfo> batchInfoList = new List<BatchInfo>();
+        public bool folded;
+    }
+
+    class BatchInfo {
+        public exMesh mesh;
+        public int index;
+        public string name;
+        public float pos_z;
+        public bool folded;
+    }
 
     // ------------------------------------------------------------------ 
     /// Register menu
@@ -34,18 +51,30 @@ class CheckBatchingWindow : EditorWindow {
 
     bool autoRefresh = false;
 
-    int materials = -1;
-    int meshes = -1;
-    int sprites = -1;
-    int activeSprites = -1;
-    int activeMeshes = -1;
+    int totalMaterialCount = -1;
+    int totalBatchCount = -1;
+    int totalSpriteCount = -1;
+    int totalActiveSpriteCount = -1;
+    int totalActiveMeshCount = -1;
+
+    List<LayerInfo> layerInfoList = new List<LayerInfo>();
+
+    AnimBool showStatistics = new AnimBool(true);
+    AnimBool showBatches = new AnimBool(true);
+    Vector2 scrollPos;
 
     // ------------------------------------------------------------------ 
     // Builtin Events 
     // ------------------------------------------------------------------ 
 
     void OnEnable() {
+        showStatistics = new AnimBool(showStatistics.target, Repaint);
+        showBatches = new AnimBool(showBatches.target, Repaint);
         Refresh();
+    }
+
+    void OnDestroy() {
+        layerInfoList.Clear();
     }
 
     void OnFocus() {
@@ -59,7 +88,25 @@ class CheckBatchingWindow : EditorWindow {
     }
 
     void OnGUI () {
+        scrollPos = EditorGUILayout.BeginScrollView(scrollPos);
+
         EditorGUILayout.Space ();
+        if (DrawOptions()) {
+            EditorGUILayout.Space ();
+            EditorGUI.indentLevel = 0;
+            if ( ex2DRenderer.instance == null ) {
+                EditorGUILayout.HelpBox("ex2DRenderer not found", MessageType.Error);
+                EditorGUILayout.EndScrollView();
+                return;
+            }
+            DrawStatistics();
+            DrawBatches();
+        }
+
+        EditorGUILayout.EndScrollView();
+    }
+
+    bool DrawOptions() {
         EditorGUI.indentLevel = 1;
 
         bool isDebug = exDebug.enabled;
@@ -67,19 +114,6 @@ class CheckBatchingWindow : EditorWindow {
         if (newIsDebug != isDebug) {
             exDebug.enabled = newIsDebug;
         }
-        EditorGUILayout.Space ();
-
-        EditorGUILayout.BeginHorizontal();
-
-        autoRefresh = EditorGUILayout.Toggle("Auto Refresh", autoRefresh);
-
-        if (!autoRefresh) {
-            if (GUILayout.Button("Refresh")) {
-                Refresh();
-            }
-        }
-
-        EditorGUILayout.EndHorizontal();
 
 #if !EX_DEBUG
         GUIStyle errorStyle = new GUIStyle();
@@ -88,64 +122,225 @@ class CheckBatchingWindow : EditorWindow {
         errorStyle.normal.textColor = Color.red;
 
         if (EditorApplication.isPlaying) {
-            EditorGUILayout.LabelField( "Unable to check batching when Debugging is disabled, \nyou must stop the game before setting it.", errorStyle );
+            EditorGUILayout.HelpBox("Unable to check batching when Debugging is disabled, \nyou must stop the game before setting it.", MessageType.Error);
         }
         else {
-            EditorGUILayout.LabelField( "Unable to check batching when Debugging is disabled.", errorStyle );
+            EditorGUILayout.HelpBox("Unable to check batching when Debugging is disabled.", MessageType.Error);
         }
-        return;
+        return false;
 #endif
 
-        if ( ex2DRenderer.instance == null ) {
-            GUILayout.Label ( "ex2DRenderer not found" );
-            return;
+        EditorGUILayout.BeginHorizontal();
+
+        autoRefresh = EditorGUILayout.Toggle("Auto Refresh", autoRefresh);
+        if (!autoRefresh) {
+            if (GUILayout.Button("Refresh")) {
+                Refresh();
+                Repaint();
+            }
+            GUILayout.FlexibleSpace();
         }
 
+        EditorGUILayout.EndHorizontal();
+        return true;
+    }
 
-        EditorGUI.indentLevel = 0;
-        EditorGUILayout.LabelField("Statistics:", EditorStyles.boldLabel);
+    void DrawStatistics() {
+        showStatistics.target = EditorGUILayout.Foldout(showStatistics.target, "Statistics:");
+        if (EditorGUILayout.BeginFadeGroup(showStatistics.faded)) {
+            ++EditorGUI.indentLevel;
 
+            string info = "";
+            info += string.Format("Materials: {0}  Meshes: {1}  Sprites: {2}  Layers: {3}", totalMaterialCount, totalBatchCount, totalSpriteCount, ex2DRenderer.instance.layerList.Count);
+            info += "\n";
+            info += string.Format("Active Meshes: {1}  Active Sprites: {0}", totalActiveSpriteCount, totalActiveMeshCount);
+            EditorGUILayout.SelectableLabel(info);
         
-        EditorGUI.indentLevel = 1;
-        string info = "";
-        info += string.Format("Materials: {0}  Meshes: {1}  Sprites: {2}  Layers: {3}", materials, meshes, sprites, ex2DRenderer.instance.layerList.Count);
-        info += "\n";
-        info += string.Format("Active Meshes: {1}  Active Sprites: {0}", activeSprites, activeMeshes);
-        EditorGUILayout.SelectableLabel(info);
+            --EditorGUI.indentLevel;
+        }
+        EditorGUILayout.EndFadeGroup();
+    }
 
-        //GUILayout.Label("Build:");
-        //GUILayout.Space (10);
+    void DrawBatches() {
+        showBatches.target = EditorGUILayout.Foldout(showBatches.target, "Batches:");
+        if (EditorGUILayout.BeginFadeGroup(showBatches.faded)) {
+            ++EditorGUI.indentLevel;
 
-        //GUILayout.BeginHorizontal();
-        //EditorGUILayout.TextArea(text);
-        //GUILayout.EndHorizontal();
+            for (int i = 0; i < layerInfoList.Count; i++) {
+                var layerInfo = layerInfoList[i];
+                var layer = layerInfo.layer;
+                if (!layer) {
+                    Refresh();
+                    Repaint();
+                    return;
+                }
+                var layerTitle = string.Format("Layer [{0}]   (Actual Materials: {1}, Meshes: {2})", layer.gameObject.name, layerInfo.materials, layerInfo.batchInfoList.Count);
+
+                EditorGUILayout.BeginHorizontal();
+
+                layerInfo.folded = EditorGUILayout.Foldout(layerInfo.folded, "");
+
+                GUILayout.Space(-24);
+                if (GUILayout.Button(layerTitle, EditorStyles.label)) {
+                    Selection.activeGameObject = layer.gameObject;
+                }
+
+                if (layer.ordered) {
+                    EditorGUILayout.LabelField("Ordered", EditorStyles.boldLabel);  // GUILayout.ExpandWidth(true)
+                }
+                else {
+                    EditorGUILayout.LabelField("Unordered");
+                }
+                GUILayout.FlexibleSpace();
+                EditorGUILayout.EndHorizontal();
+
+
+                if (layerInfo.folded) {
+                    ++EditorGUI.indentLevel;
+
+                    for (int j = 0; j < layerInfo.batchInfoList.Count; j++) {
+                        var batchInfo = layerInfo.batchInfoList[j];
+                        var mesh = batchInfo.mesh;
+                        if (!mesh) {
+                            Refresh();
+                            Repaint();
+                            return;
+                        }
+                        var batchTitle = string.Format("Mesh [{0}]   (Sprites: {3}, Index: {1}, Z: {2})", batchInfo.name, batchInfo.index, batchInfo.pos_z, mesh.sortedSpriteList.Count);
+
+                        EditorGUILayout.BeginHorizontal();
+                        batchInfo.folded = EditorGUILayout.Foldout(batchInfo.folded, "");
+                        GUILayout.Space(-10);
+                        if (GUILayout.Button(batchTitle, EditorStyles.label)) {
+                            Selection.activeObject = mesh;
+                        }
+                        GUILayout.FlexibleSpace();
+                        EditorGUILayout.EndHorizontal();
+
+                        if (batchInfo.folded) {
+                            ++EditorGUI.indentLevel;
+
+                            for (int k = 0; k < mesh.sortedSpriteList.Count; k++) {
+                                var sprite = mesh.sortedSpriteList[k];
+                                if (!sprite) {
+                                    Refresh();
+                                    Repaint();
+                                    return;
+                                }
+                                var spriteTitle = string.Format("Sprite [{0}]   (Depth: {1}, GlobalDepth: {2})", sprite.gameObject.name, sprite.depth, (sprite as exLayer.IFriendOfLayer).globalDepth);
+
+                                EditorGUILayout.BeginHorizontal();
+                                GUILayout.Space(65);
+                                if (GUILayout.Button(spriteTitle, EditorStyles.label)) {
+                                    Selection.activeObject = sprite;
+                                }
+                                EditorGUILayout.EndHorizontal();
+                            }
+
+                            --EditorGUI.indentLevel;
+                        }
+                    }
+
+                    --EditorGUI.indentLevel;
+                }
+            }
+            
+            --EditorGUI.indentLevel;
+        }
+        EditorGUILayout.EndFadeGroup();
+    }
+
+    string GetBatchName(exMesh mesh) {
+        string matName;
+        Material mat = mesh.material;
+        if (mat != null) {
+            if (mat.mainTexture) {
+                matName = mat.mainTexture.name;
+            }
+            else {
+                matName = mat.name;
+            }
+        }
+        else {
+            matName = "None";
+        }
+        return matName;
     }
 
     void Refresh() {
         if (!autoRefresh) {
             //Debug.Log("CheckBatching Refreshed");
         }
-        materials = ex2DRenderer.GetMaterialCount();
+#if EX_DEBUG
+        totalMaterialCount = ex2DRenderer.GetMaterialCount();
+#endif
+        totalBatchCount = 0;
+        totalSpriteCount = 0;
+        totalActiveMeshCount = 0;
+        totalActiveSpriteCount = 0;
 
-        meshes = 0;
-        sprites = 0;
-        activeMeshes = 0;
-        activeSprites = 0;
+        var layerIndex = 0;
+
         var layers = ex2DRenderer.instance.layerList;
         foreach (var layer in layers) {
-            meshes += layer.meshList.Count;
+            if (!layer) {
+                continue;
+            }
+
+            LayerInfo layerInfo;
+            if (layerIndex < layerInfoList.Count) {
+                // keep folded unchanged
+                layerInfo = layerInfoList[layerIndex];
+            }
+            else {
+                layerInfo = new LayerInfo();
+                layerInfoList.Add(layerInfo);
+            }
+            layerIndex++;
+
+            layerInfo.layer = layer;
+
+            HashSet<Material> materialCounter = new HashSet<Material>();
+            var batchIndex = 0;
             foreach (var mesh in layer.meshList) {
-                if (mesh.gameObject.activeSelf) {
-                    ++activeMeshes;
+                if (!mesh) {
+                    continue;
                 }
-                sprites += mesh.sortedSpriteList.Count;
+                ++totalBatchCount;
+                if (mesh.material) {
+                    materialCounter.Add(mesh.material);
+                }
+
+                BatchInfo batchInfo;
+                if (batchIndex < layerInfo.batchInfoList.Count) {
+                    // keep folded unchanged
+                    batchInfo = layerInfo.batchInfoList[batchIndex];
+                }
+                else {
+                    batchInfo = new BatchInfo();
+                    layerInfo.batchInfoList.Add(batchInfo);
+                }
+                batchIndex++;
+
+                batchInfo.mesh = mesh;
+                batchInfo.index = totalBatchCount;
+                batchInfo.name = GetBatchName(mesh);
+                batchInfo.pos_z = mesh.transform.position.z;
+
+                if (mesh.gameObject.activeSelf) {
+                    ++totalActiveMeshCount;
+                }
+                totalSpriteCount += mesh.sortedSpriteList.Count;
 
                 foreach (var sprite in mesh.sortedSpriteList) {
                     if (sprite.visible) {
-                        ++activeSprites;
+                        ++totalActiveSpriteCount;
                     }
 	            }
 	        }
+            layerInfo.batchInfoList.RemoveRange(batchIndex, layerInfo.batchInfoList.Count - batchIndex);
+            layerInfo.materials = materialCounter.Count;
         }
+        layerInfoList.RemoveRange(layerIndex, layerInfoList.Count - layerIndex);
     }
 }
